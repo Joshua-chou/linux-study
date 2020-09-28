eb8ba2cd6eefbe816a3871d9d3569e0ddef9dce5
Joshua-chou <2295095440@qq.com>
2020-09-28 09:10:49 +0800

    添加“week2”的学习内容和代码

---
+++ week2/code/week2-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+        int i = 1,j = 1;
+        for(; i<10; i++){
+                j = 1;
+                for(; j<10; j++){
+                        if(j<i)continue;
+                        printf("%d * %d = %d;",i,j,i*j);
+                }
+                printf("\n");
+        }
+        return 0;
+}
